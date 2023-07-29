07be90d55ba5b089a572a8c7096c9e5f64ce3222
Praween-em <128447489+Praween-em@users.noreply.github.com>
2023-07-29 11:55:17 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+
+class Primecal {
+    public boolean isPrime(BigInteger input) {
+        BigInteger two = BigInteger.valueOf(2);
+        BigInteger i = two;
+        while (i.compareTo(input.divide(two)) <= 0) {
+            if (input.mod(i).equals(BigInteger.ZERO)) {
+                return false;
+            }
+            i = i.add(BigInteger.ONE);
+        }
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        long n = 600;
+        String filename= "Prime.txt";
+
+        Primecal primecal = new Primecal();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (long i = 2; i < n; i++) {
+                if (primecal.isPrime(BigInteger.valueOf(i))) {
+                    System.out.println(i);
+                    //BigInteger result = BigInteger.valueOf(2).pow((int)i).subtract(BigInteger.ONE);
+                   // System.out.println("Result for " + i + ": " + result);
+                    writer.write(i + "\n"); // Write the prime number to the file
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
